42bbb6610bc5c1e1eb12d4df21ea01a2ba9e4129
Suri-Feng <0924suri@gmail.com>
2023-03-07 20:53:52 -0800

    refactor

--- src/main/java/project/TestShutDown.java
+++ src/main/java/project/TestShutDown.java
@@ -1,34 +1,33 @@
-//package com.g3.CPEN431.project;
-//
-//import com.g3.CPEN431.project.ServerInfo.Server;
-//import com.g3.CPEN431.project.ServerInfo.ServerList;
-//import com.g3.CPEN431.project.Test.OutcomePair;
-//import com.g3.CPEN431.project.Test.TestHandler;
-//import org.junit.Test;
-//
-//import com.g3.CPEN431.project.Client.UDPClient;
-//
-//import java.io.IOException;
-//
-//
-//public class TestShutDown {
-//    @Test
-//    public void ShutDown() throws IOException, InterruptedException {
-//        TestHandler testHandler = new TestHandler();
-//        UDPClient client = testHandler.clientList.getFirstClient();
-//        Server server = testHandler.serverList.getFirstServer();
-//
-//        OutcomePair outcome0 = testHandler.isAlive(client, server);
-//        testHandler.printOutcome(outcome0); // Expect SUCCESS
-//
-//        Thread.sleep(1000);
-//
-//        OutcomePair outcome1 = testHandler.shutdown(client, server);
-//        testHandler.printOutcome(outcome1); // Expect TIMEOUT
-//
-//        Thread.sleep(1000);
-//
-//        OutcomePair outcome2 = testHandler.isAlive(client, server);
-//        testHandler.printOutcome(outcome2); // Expect TIMEOUT
-//    }
-//}
+package project;
+
+import project.ServerInfo.Server;
+import project.Test.OutcomePair;
+import project.Test.TestHandler;
+
+
+import project.Client.UDPClient;
+
+import java.io.IOException;
+
+
+public class TestShutDown {
+
+    public void ShutDown(String serverList) throws IOException, InterruptedException {
+        TestHandler testHandler = new TestHandler(serverList, 1);
+        UDPClient client = testHandler.clientList.getFirstClient();
+        Server server = testHandler.serverList.getFirstServer();
+
+        OutcomePair outcome0 = testHandler.isAlive(client, server);
+        testHandler.printOutcome(outcome0); // Expect SUCCESS
+
+        Thread.sleep(1000);
+
+        OutcomePair outcome1 = testHandler.shutdown(client, server);
+        testHandler.printOutcome(outcome1); // Expect TIMEOUT
+
+        Thread.sleep(1000);
+
+        OutcomePair outcome2 = testHandler.isAlive(client, server);
+        testHandler.printOutcome(outcome2); // Expect TIMEOUT
+    }
+}
--- src/main/java/project/TestStopAndResume.java
+++ src/main/java/project/TestStopAndResume.java
@@ -1,8 +1,5 @@
 package project;
 
-import com.g3.CPEN431.A7.Model.Distribution.Node;
-import com.g3.CPEN431.A7.Model.Distribution.NodesCircle;
-import com.g3.CPEN431.A7.Model.KVServer;
 import project.Client.UDPClient;
 import project.ServerInfo.Server;
 import project.Test.OutcomePair;
@@ -16,46 +13,44 @@ public class TestStopAndResume {
 
     public static void main( String[] args ) throws IOException, InterruptedException {
         //"/Users/suri/Desktop/CPEN431/test/servers.txt"
-        StopAndResumeWithGet(args[0]);
+        StopAndResumeWithGet(args[0]); // pass in servers.txt
     }
 
-//    public void StopAndResume() throws IOException, InterruptedException {
-//        TestHandler testHandler = new TestHandler();
-//        UDPClient client = testHandler.clientList.getFirstClient();
-//        Server server = testHandler.serverList.getFirstServer();
-//
-//        // Stage 0
-//        OutcomePair outcome0 = testHandler.isAlive(client, server);
-//        testHandler.printOutcome(outcome0); // Expect SUCCESS
-//        OutcomePair outcome1 = testHandler.getPid(client, server);
-//        testHandler.printOutcome(outcome1); // Expect SUCCES
-//
-//        // Stage 1
-//        // TODO: Will this send a shut down request?
-//        OutcomePair outcome2 = testHandler.processControlShutDown(client, server);
-//        testHandler.printOutcome(outcome2); // Expect SUCCESS
-//        Thread.sleep(1000);
-//        OutcomePair outcome3 = testHandler.isAlive(client, server);
-//        testHandler.printOutcome(outcome3); // Expect TIMEOUT
-//
-//        // Stage 2
-//        OutcomePair outcome4 = testHandler.processControlResume(client, server);
-//        testHandler.printOutcome(outcome4); // Expect SUCCESS
-//        Thread.sleep(1000);
-//        OutcomePair outcome5 = testHandler.isAlive(client, server);
-//        testHandler.printOutcome(outcome5); // Expect SUCCESS
-//    }
+    public void StopAndResume(String serverList)  throws IOException, InterruptedException {
+        TestHandler testHandler = new TestHandler(serverList, 1);
+        UDPClient client = testHandler.clientList.getFirstClient();
+        Server server = testHandler.serverList.getFirstServer();
 
+        // Stage 0
+        OutcomePair outcome0 = testHandler.isAlive(client, server);
+        testHandler.printOutcome(outcome0); // Expect SUCCESS
+        OutcomePair outcome1 = testHandler.getPid(client, server);
+        testHandler.printOutcome(outcome1); // Expect SUCCES
+
+        // Stage 1
+        // This will not send a shut-down request
+        OutcomePair outcome2 = testHandler.processControlShutDown(client, server);
+        testHandler.printOutcome(outcome2); // Expect SUCCESS
+        Thread.sleep(1000);
+        OutcomePair outcome3 = testHandler.isAlive(client, server);
+        testHandler.printOutcome(outcome3); // Expect TIMEOUT
+
+        // Stage 2
+        OutcomePair outcome4 = testHandler.processControlResume(client, server);
+        testHandler.printOutcome(outcome4); // Expect SUCCESS
+        Thread.sleep(1000);
+        OutcomePair outcome5 = testHandler.isAlive(client, server);
+        testHandler.printOutcome(outcome5); // Expect SUCCESS
+    }
+
+
+    // TODO: 1. add expect success rate; 2. add new keys StopAndResumeWithNewKeyGet
     public static void StopAndResumeWithGet(String serverList) throws IOException, InterruptedException {
         TestHandler testHandler = new TestHandler(serverList, 1);
         UDPClient client = testHandler.clientList.getFirstClient();
         ArrayList<Server> servers = testHandler.serverList.servers;
 
         Server server0= servers.get(0);
-        //OutcomePair outcome2 = testHandler.put(client, server0, "K1", "1", 0);
-        //testHandler.printOutcome(outcome2); // EXPECT [K-N-F, NULL], [TIMEOUT, NULL], [SUCCESS, NULL]
-        //OutcomePair outcome3 = testHandler.get(client, server0, "K1");
-        //testHandler.printOutcome(outcome3); // EXPECT GET(K1) -> [TIMEOUT, NULL], [K-N-F, NULL]
         OutcomePair outcome22 = testHandler.put(client, server0, "K1", "2", 0);
         testHandler.printOutcome(outcome22); // EXPECT [K-N-F, NULL], [TIMEOUT, NULL], [SUCCESS, NULL]
         OutcomePair outcome33 = testHandler.get(client, server0, "K1");
@@ -70,8 +65,6 @@ public class TestStopAndResume {
 
             for (Server anotherServer: servers) {
                 if (anotherServer.getPort() != server.getPort()) {
-                    //OutcomePair outcome7 = testHandler.get(client, anotherServer, "K1");
-                    //testHandler.printOutcome(outcome7); // EXPECT GET(K1) -> [TIMEOUT, NULL], [K-N-F, NULL]
                     OutcomePair outcome8 = testHandler.get(client, anotherServer, "K1");
                     testHandler.printOutcome(outcome8); // EXPECT GET(K1) -> [TIMEOUT, NULL], [K-N-F, NULL]
                 }
@@ -82,8 +75,6 @@ public class TestStopAndResume {
             testHandler.printOutcome(outcome6); // Expect TIMEOUT
 
             for (Server anotherServer: servers) {
-                //OutcomePair outcome7 = testHandler.get(client, anotherServer, "K1");
-                //testHandler.printOutcome(outcome7); // EXPECT GET(K1) -> [TIMEOUT, NULL], [K-N-F, NULL]
                 OutcomePair outcome8 = testHandler.get(client, anotherServer, "K1");
                 testHandler.printOutcome(outcome8); // EXPECT GET(K1) -> [TIMEOUT, NULL], [K-N-F, NULL]
             }
